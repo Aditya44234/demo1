4b4f87110bcc4d9057a65e5a7c560d058da245f2
Aditya joshi <168006690+Aditya44234@users.noreply.github.com>
2025-02-20 18:33:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let btn=document.querySelector(".gamesBtn");
+let list=document.querySelector(".list");
+let li=document.querySelector(".li");
+let cover=document.querySelector(".cover");
+
+// let body=document.querySelector("body");
+
+
+
+
+
+
+btn.onclick=()=>{
+        list.classList.toggle("active");
+       
+}
+
+
+
+
+
